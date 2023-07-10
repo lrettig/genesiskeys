c1d64798c41f363fedf9cbd54cb0092e1cac15af
Lane Rettig <lanerettig@gmail.com>
2023-07-10 11:59:36 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module genesiskeys
+
+go 1.19
+
+require golang.org/x/crypto v0.11.0
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/crypto/ed25519"
+)
+
+// Placeholder function: replace this with your function
+func processKeys(keys []ed25519.PublicKey, m, n uint) string {
+	return "Result" // replace with your processing
+}
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		log.Fatal("Please provide one CSV file as an argument")
+	}
+
+	filePath := flag.Args()[0]
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Could not open the CSV file: ", err)
+	}
+
+	r := csv.NewReader(file)
+
+	// Initialize the line number
+	line := 0
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal("Could not read the CSV file: ", err)
+		}
+
+		// Increment the line number for each record
+		line++
+
+		name := record[0]
+
+		amountStr := strings.ReplaceAll(record[1], ",", "")
+		amount, err := strconv.ParseUint(amountStr, 10, 64)
+		if err != nil {
+			log.Printf("Warning: Invalid amount for record at line %d: %s\n", line, name)
+			continue
+		}
+
+		var keys []ed25519.PublicKey
+		for _, keyStr := range record[4:8] {
+			if keyStr == "" {
+				continue
+			}
+
+			keyBytes := []byte(keyStr) // replace with your own decoding if necessary
+			if len(keyBytes) != ed25519.PublicKeySize {
+				log.Printf("Error: Invalid key for record at line %d: %s\n", line, name)
+				continue
+			}
+
+			keys = append(keys, ed25519.PublicKey(keyBytes))
+		}
+
+		mStr, nStr := record[6], record[7]
+		var m, n uint
+		if mStr != "" || nStr != "" {
+			if mStr == "" || nStr == "" {
+				log.Printf("Error: Only one of m or n is specified for record at line %d: %s\n", line, name)
+				continue
+			}
+
+			mUint64, err := strconv.ParseUint(mStr, 10, 64)
+			if err != nil {
+				log.Printf("Error: Invalid m for record at line %d: %s\n", line, name)
+				continue
+			}
+			m = uint(mUint64)
+
+			nUint64, err := strconv.ParseUint(nStr, 10, 64)
+			if err != nil {
+				log.Printf("Error: Invalid n for record at line %d: %s\n", line, name)
+				continue
+			}
+			n = uint(nUint64)
+
+			if int(n) != len(keys) {
+				log.Printf("Error: n does not match the number of keys for record at line %d: %s\n", line, name)
+				continue
+			}
+		}
+
+		result := processKeys(keys, m, n)
+		fmt.Println(strings.Join([]string{name, strconv.FormatUint(amount, 10), result}, ","))
+	}
+}
